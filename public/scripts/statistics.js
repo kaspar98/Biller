f1c6d5ed0045314da9afcc015420b7b106c50614
Kaspar Valk <valk.kaspar@gmail.com>
2018-10-15 00:28:17 +0300

    Statistics chart bugfix

--- public/scripts/statistics.js
+++ public/scripts/statistics.js
@@ -1,10 +1,11 @@
 window.onload = function () {
     var dataPoints = [];
     for(i=0;i<24;i++){
+        i = parandaFormaati(i, 2);
         if(document.getElementById(i)){
-            dataPoints.push({ x: i+1, y: parseInt(document.getElementById(i).innerText), label: i})
+            dataPoints.push({ x: parseInt(i)+1, y: parseInt(document.getElementById(i).innerText), label: i})
         } else {
-            dataPoints.push({ x: i+1, y: 0, label: i})
+            dataPoints.push({ x: parseInt(i)+1, y: 0, label: i})
         }
     }
     try {
@@ -22,3 +23,14 @@ window.onload = function () {
         script.src = "https://cdnjs.cloudflare.com/ajax/libs/canvasjs/1.7.0/canvasjs.js";
         document.getElementsByTagName('head')[0].appendChild(script);    }
 };
+
+
+
+function parandaFormaati(number, length) {
+    var nr = '' + number;
+    while (nr.length < length) {
+        nr = '0' + nr;
+    }
+    return nr;
+}
+
