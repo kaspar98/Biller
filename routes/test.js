62930827cf8c8efdfadae78ca8472df0f5f8f9a6
Oliver <o.vainumae@gmail.com>
2018-10-15 01:44:07 +0300

    title,desc,keywords

--- routes/test.js
+++ routes/test.js
@@ -4,6 +4,10 @@ const script = "../scripts/test.js";
 
 router.get('/', function(req, res, next) {
     res.render('test', {
+        title: "Testimine",
+        description: "Sellel lehel saad vajutada nupukest, mis teise elemendi värvi ja suurust muudab." +
+            "Lisaks saad kaardikest ka vaadata",
+        keywords: "Biller, nupp, värvid, kaart",
         script: [script]
     });
 });
