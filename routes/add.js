839cd5f4802c915610d88d65f98aa657a9d8aa68
Oliver <o.vainumae@gmail.com>
2018-10-15 00:52:21 +0300

    title, description, keywords commentitena

--- routes/add.js
+++ routes/add.js
@@ -3,13 +3,21 @@ const router = express.Router();
 const db = require("../db/mysql");
 const {ensureAuthenticated} = require("../helpers/auth");
 
+/*router.get('/', (req, res, next) => {
+    res.render('add', {
+        title: "Tulemused",
+        description: "See leht on sõprade otsimiseks. Siin saad otsingust leitud sõpru lisada.",
+        keywords: "Biller, otsing, tulemused, sõbrad, sõber, lisamine"
+    });
+});*/
+
 router.post('/', ensureAuthenticated, (req, res) => {
     let jupid = req.body.findName.split(" ");
     let last = jupid[jupid.length - 1];
     let first = jupid.join(" ").replace(" " + last, "");
     db.getUserByName(first, last, req.user[0]["id"], (err, results) => {
         for (i = 0; i < results.length; i++) {
-            if(results[i]["id"] == req.user[0]["id"]){
+            if (results[i]["id"] == req.user[0]["id"]) {
                 results.splice(i, 1);
             }
         }
