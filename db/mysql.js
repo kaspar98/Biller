2310c2208b06abaf800bebb8d19f96df6f38ed1a
kaspar98 <valk.kaspar@gmail.com>
2018-10-15 15:31:29 +0300

    MYSQL bugfix

--- db/mysql.js
+++ db/mysql.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const fs = require('fs');
 const path = require("path");
 
-const pool  = mysql.createPool({
+const pool = mysql.createPool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -13,12 +13,12 @@ const pool  = mysql.createPool({
 
 function addUser(firstName, lastName, email, username, password, googleID, cb) {
     var sql = "CALL sp_new_user('" + firstName + "', '" + lastName + "', '" + email + "', '" +
-        username + "', '" + password + "', '"+googleID+"');";
+        username + "', '" + password + "', '" + googleID + "');";
     pool.query(sql, cb);
 }
 
 function addFriend(uid, fid, cb) {
-    var sql = "CALL sp_add_friend('"+uid+"', '"+fid+"');";
+    var sql = "CALL sp_add_friend('" + uid + "', '" + fid + "');";
     pool.query(sql, cb);
 }
 
@@ -32,34 +32,34 @@ function getUserByEmail(email, cb) {
 
 function getUserByName(firstName, lastName, uid, cb) {
     // Hetkel tagastab inimesi, kellel puudub kastuajaga sõprus
-    pool.query("SELECT firstName, lastName, v_users.id, username FROM v_users WHERE firstName='"+firstName+"' AND lastName='"+lastName+"' NOT EXISTS (SELECT * FROM v_friends " +
-        "WHERE (id1=v_users.id AND id2='"+uid+"') OR (id1='"+uid+"' AND id2=v_users.id))", cb);
+    pool.query("SELECT firstName, lastName, v_users.id, username FROM v_users WHERE firstName='" + firstName + "' AND lastName='" + lastName + "' AND NOT EXISTS (SELECT * FROM v_friends " +
+        "WHERE (id1=v_users.id AND id2='" + uid + "') OR (id1='" + uid + "' AND id2=v_users.id))", cb);
 }
 
 function getUserByGoogleID(googleID, cb) {
     pool.query("SELECT * FROM v_users WHERE googleID=?", googleID, cb);
 }
 
-function getFriendRequests(uid, cb){
+function getFriendRequests(uid, cb) {
     pool.query("SELECT firstName, lastName, username, id FROM v_friends, v_users WHERE id1=id AND id2=? AND confirmed=0", uid, cb);
 }
 
-function getFriends(uid, cb){
-    pool.query("SELECT firstName, lastName, username, id FROM v_friends, v_users WHERE (id1='"+uid+"' OR id2='"+uid+"') " +
-        "AND confirmed=1 AND id<>'"+uid+"' AND (id1=id OR id2=id)", cb);
+function getFriends(uid, cb) {
+    pool.query("SELECT firstName, lastName, username, id FROM v_friends, v_users WHERE (id1='" + uid + "' OR id2='" + uid + "') " +
+        "AND confirmed=1 AND id<>'" + uid + "' AND (id1=id OR id2=id)", cb);
 }
 
-function changeFriendRequestStatus(uid, fid, status, cb){
-    pool.query("CALL sp_changeFriendRequestStatus('"+uid+"','"+fid+"','"+status+"')", cb);
+function changeFriendRequestStatus(uid, fid, status, cb) {
+    pool.query("CALL sp_changeFriendRequestStatus('" + uid + "','" + fid + "','" + status + "')", cb);
 }
 
 function addEvent(title, descriptionIn, creatorId, cb) {
-    var sql = "CALL sp_addEvent('"+title+"', '"+descriptionIn+"', '"+creatorId+"')";
+    var sql = "CALL sp_addEvent('" + title + "', '" + descriptionIn + "', '" + creatorId + "')";
     pool.query(sql, cb);
 }
 
 function getEvents(uid, cb) {
-    var sql = "SELECT * FROM v_events_and_payments WHERE creatorId='"+uid+"' OR idFrom='"+uid+"' OR idTo='"+uid+"'";
+    var sql = "SELECT * FROM v_events_and_payments WHERE creatorId='" + uid + "' OR idFrom='" + uid + "' OR idTo='" + uid + "'";
     pool.query(sql, cb);
 }
 
@@ -70,38 +70,38 @@ function getEmptyEvents(uid, cb) {
     pool.query(sql, uid, cb);
 }
 
-function addPayment(toid, fromusername, amountIn, eventid, cb){
-    var sql = "CALL sp_addPayment('"+toid+"', '"+fromusername+"', '"+amountIn+"', '"+eventid+"')";
+function addPayment(toid, fromusername, amountIn, eventid, cb) {
+    var sql = "CALL sp_addPayment('" + toid + "', '" + fromusername + "', '" + amountIn + "', '" + eventid + "')";
     pool.query(sql, cb);
 }
 
-function getPayments(uid, cb){
-    var sql = "SELECT * FROM v_payments, v_users WHERE (idFrom='"+uid+"' OR idTo='"+uid+"')" +
-        "AND (idFrom=v_users.id OR idTo=v_users.id) AND v_users.id <> '"+uid+"' AND confirmed=1";
+function getPayments(uid, cb) {
+    var sql = "SELECT * FROM v_payments, v_users WHERE (idFrom='" + uid + "' OR idTo='" + uid + "')" +
+        "AND (idFrom=v_users.id OR idTo=v_users.id) AND v_users.id <> '" + uid + "' AND confirmed=1";
     pool.query(sql, cb);
 }
 
-function countFriends(uid, cb){
-    var sql = "SELECT COUNT(*) FROM Friends WHERE confirmed=1 AND (id1='"+uid+"' OR id2='"+uid+"')";
+function countFriends(uid, cb) {
+    var sql = "SELECT COUNT(*) FROM Friends WHERE confirmed=1 AND (id1='" + uid + "' OR id2='" + uid + "')";
     pool.query(sql, cb);
 }
 
-function changePaymentStatus(id, status, cb){
-    var sql = "CALL sp_changePaymentStatus('"+id+"', '"+status+"')";
+function changePaymentStatus(id, status, cb) {
+    var sql = "CALL sp_changePaymentStatus('" + id + "', '" + status + "')";
     pool.query(sql, cb);
 }
 
 function addStatistics(landingPage, landingTime, landingDate, browser, os, cb) {
-    var sql = "CALL sp_addStatistics('"+landingPage+"', '"+landingTime+"', '"+landingDate+"', '"+browser+"', '"+os+"')";
+    var sql = "CALL sp_addStatistics('" + landingPage + "', '" + landingTime + "', '" + landingDate + "', '" + browser + "', '" + os + "')";
     pool.query(sql, cb);
 }
 
-function getStatistics(cb){
+function getStatistics(cb) {
     pool.query("SELECT * FROM v_statistics", cb);
 }
 
 
-function init () {
+function init() {
     data = fs.readFileSync(path.join(__dirname, '/init.sql'), 'utf8');
     pool.query(data, (error) => {
         if (error) {
