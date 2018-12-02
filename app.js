74be89340df2ec4684b8c2d7668c0cfb953c156e
Oliver <o.vainumae@gmail.com>
2018-12-02 18:58:26 +0200

    Expires headers cleanup2

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const fs = require("fs");
 const browser = require('browser-detect');
 const db = require("./db/mysql");
 const app = express();
-const serveStatic = require("serve-static");
 
 const port = process.env.PORT || 5000;
 
